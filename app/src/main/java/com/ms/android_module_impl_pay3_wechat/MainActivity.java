2873d41d7c4f24e585f252e7b2f0e3f01b1043f4
m_maohuawei <maohuawei@bdlbsc.com>
2020-09-16 17:47:30 +0800

    init

---
+++ app/src/main/java/com/ms/android_module_impl_pay3_wechat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ms.android_module_impl_pay3_wechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ sdk/src/main/java/com/ms/module/impl/pay3/wechat/WeChatPayImpl.java
@@ -0,0 +1,4 @@
+package com.ms.module.impl.pay3.wechat;
+
+public class WeChatPayImpl {
+}
